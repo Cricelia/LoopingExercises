f093dc76fa1ff6d215000da015f1cd157be7e850
Cricelia <cpradoperez@outlook.com>
2025-04-22 00:28:15 -0700

    Mod 3 - Exercise 1
    Created a class "WhileLoop", added main() method and created a while loop to display " I love Java". Created a class "DoWhileLoop", added main() method and created a  do/while loop to display " I love Java". Created a class "ForLoop", added main() method and used a for loop to display a countdown from 10 to 1.

---
+++ src/main/java/org/example/WhileLoop.java
@@ -0,0 +1,15 @@
+package org.example;
+
+
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int i = 0;
+        while(i <= 4) {
+            System.out.println("I love Java");
+            i++;
+        }
+
+        }
+    }
+
